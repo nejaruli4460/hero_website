3fde730a1b1be855e5901e2edd3e34e25f6ec99e
nejaruli4460 <79055019+nejaruli4460@users.noreply.github.com>
2021-04-10 17:55:11 +0530

    done

---
+++ script.js
@@ -0,0 +1,122 @@
+function myfun1()
+{
+    let n =document.getElementById("body");
+    let x =document.getElementById("button1");
+    let o =document.getElementById("button2");
+    let p =document.getElementById("button3");
+    let q =document.getElementById("button4");
+    let  r=document.getElementById("button5");
+    let s =document.getElementById("button6");
+    n.style.backgroundImage="url(download.jpg)";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+        p.style.border="0px";
+
+}
+function myfun2()
+{
+    let n =document.getElementById("body");
+    let o =document.getElementById("button1");
+    let x =document.getElementById("button2");
+    let p =document.getElementById("button3");
+    let q =document.getElementById("button4");
+    let  r=document.getElementById("button5");
+    let s =document.getElementById("button6");
+    n.style.backgroundImage="url(https://img.edilportale.com/product-thumbs/b_heritage-solid-color-wallpaper-nanni-giancarlo-c-356684-relaa3aafd2.jpg)";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+    p.style.border="0px";
+}
+function myfun3()
+{
+    let n =document.getElementById("body");
+    let o =document.getElementById("button1");
+    let p =document.getElementById("button2");
+    let x =document.getElementById("button3");
+    let q =document.getElementById("button4");
+    let  r=document.getElementById("button5");
+    let s =document.getElementById("button6");
+    n.style.backgroundImage="url(https://wallpapercave.com/wp/wp1920741.jpg)";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+    p.style.border="0px";
+}
+function myfun4()
+{
+    let n =document.getElementById("body");
+    let o =document.getElementById("button1");
+    let q =document.getElementById("button2");
+    let p =document.getElementById("button3");
+    let x =document.getElementById("button4");
+    let  r=document.getElementById("button5");
+    let s =document.getElementById("button6");
+    n.style.backgroundImage="url(https://cdn.hipwallpaper.com/i/30/79/uxEnq5.png";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+    p.style.border="0px";
+}
+function myfun5()
+{
+    let n =document.getElementById("body");
+    let o =document.getElementById("button1");
+    let q =document.getElementById("button2");
+    let p =document.getElementById("button3");
+    let r =document.getElementById("button4");
+    let  x=document.getElementById("button5");
+    let s =document.getElementById("button6");
+    n.style.backgroundImage="url(https://images.wallpaperscraft.com/image/solid_color_bright_lines_18572_1366x768.jpg";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+    p.style.border="0px";
+}
+function myfun6()
+{
+    let n =document.getElementById("body");
+    let o =document.getElementById("button1");
+    let q =document.getElementById("button2");
+    let p =document.getElementById("button3");
+    let r =document.getElementById("button4");
+    let  s=document.getElementById("button5");
+    let x =document.getElementById("button6");
+    n.style.backgroundImage="url(https://wonderfulengineering.com/wp-content/uploads/2014/07/background-wallpapers-10.jpg";
+    n.style.backgroundRepeat="no-repeat";
+    n.style.backgroundAttachment="fixed";
+    n.style.backgroundSize="cover";
+    x.style.border="2px solid white";  
+    o.style.border="0px"  ;
+    q.style.border="0px"  ;
+    r.style.border="0px"  ;
+    s.style.border="0px"  ;  
+    p.style.border="0px";
+}
+
